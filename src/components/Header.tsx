b4d462227436b36784f4dfaa385c896c82274952
Landon Johnson <landonjohnsontechnologies@gmail.com>
2022-09-26 19:39:06 -0600

    [feat] header icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { FC, ForwardedRef, forwardRef } from "react";
 import { useStore } from "../hooks/use-store";
 import styles from "./Header.module.scss";
+import { DashboardIcon, PrintIcon, SunIcon } from "./icons";
 
 interface HeaderProps {
   onPrint: () => void;
@@ -8,20 +9,33 @@ interface HeaderProps {
 }
 
 export const Header: FC<HeaderProps> = forwardRef(({ onPrint }, ref) => {
-  const { changeTheme } = useStore();
+  const { setStore, changeTheme } = useStore();
 
   return (
     <header className={styles.root}>
       <b>ljdocument</b>
       <div className={styles.base}>
+        <button
+          ref={ref}
+          id="theme"
+          onClick={() =>
+            setStore((currStore) => ({
+              ...currStore,
+              layout: currStore.layout === "card" ? "resume" : "card",
+            }))
+          }
+        >
+          <DashboardIcon width={16} height={16} /> Layout
+        </button>
         <button ref={ref} id="theme" onClick={changeTheme}>
-          ☀ Theme
+          <SunIcon width={16} height={16} />
+          Theme
           <div>
             <kbd>CTRL</kbd> + <kbd>K</kbd>
           </div>
         </button>
         <button onClick={onPrint}>
-          🖨 Print
+          <PrintIcon width={16} height={16} /> Print
           <div>
             <kbd>CTRL</kbd> + <kbd>P</kbd>
           </div>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,22 +2,28 @@ import { ReactComponent as AddIcon } from "./add.svg";
 import { ReactComponent as ArrowUpIcon } from "./arrow-down.svg";
 import { ReactComponent as ArrowDownIcon } from "./arrow-up.svg";
 import { ReactComponent as CalendarIcon } from "./calendar.svg";
+import { ReactComponent as DashboardIcon } from "./dashboard.svg";
 import { ReactComponent as DeleteIcon } from "./delete.svg";
 import { ReactComponent as DotsIcon } from "./dots.svg";
 import { ReactComponent as EditIcon } from "./edit.svg";
 import { ReactComponent as EmailIcon } from "./email.svg";
 import { ReactComponent as LocationIcon } from "./location.svg";
 import { ReactComponent as PhoneIcon } from "./phone.svg";
+import { ReactComponent as PrintIcon } from "./print.svg";
+import { ReactComponent as SunIcon } from "./sun.svg";
 
 export {
   AddIcon,
-  ArrowUpIcon,
   ArrowDownIcon,
+  ArrowUpIcon,
   CalendarIcon,
+  DashboardIcon,
   DeleteIcon,
   DotsIcon,
   EditIcon,
   EmailIcon,
   LocationIcon,
   PhoneIcon,
+  PrintIcon,
+  SunIcon,
 };
